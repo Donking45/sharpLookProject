49d4544a546cd8b183c6d6be280841d5b5a1fc2d
Donking45 <kingsleyokon610@gmail.com>
2025-07-07 08:02:25 +0100

    updates

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,5 +1,4 @@
-const multer = require('multer');
-const { upload } = require('../middlewares/authMiddleware'); // adjust path as needed
+const { sendEmail} = require('../utils/sendMail')
 const Vendor = require('../models/vendorModel');
 //const bcrypt = require('bcryptjs');
 
