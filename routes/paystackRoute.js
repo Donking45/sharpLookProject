93cf98cc484140aac298adc4bc90e6f549fd6178
Donking45 <kingsleyokon610@gmail.com>
2025-07-10 06:11:31 +0100

    updates

--- routes/paystackRoute.js
+++ routes/paystackRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { initializePayment } = require('../controllers/paystackController');
 
-router.post('/initiate', initializePayment);
+router.post('/paystack/initiatePayment', initializePayment);
 
 module.exports = router;
