10f0c0c919d316cab4d631458592ce509fc5222f
Donking45 <kingsleyokon610@gmail.com>
2025-07-14 09:51:31 +0100

    updates

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -2,6 +2,7 @@ const { sendEmail} = require('../utils/sendMail')
 const Vendor = require('../models/vendorModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 
 const vendorRegistration = async (req, res) => {
