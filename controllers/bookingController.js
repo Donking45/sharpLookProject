ec60549fc9ebfb89204c91a72a64a9548b6b04db
Donking45 <kingsleyokon610@gmail.com>
2025-07-23 05:41:21 +0100

    updates

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,10 +1,11 @@
 const Booking = require('../models/bookingModel');
 const Vendor = require('../models/vendorModel');
 
+
 const createBookings = async (req, res) => {
   try {
-    const { vendorId, serviceType, appointmentDate, timeSlot, location} = req.body;
-    const clientId = req.user.id;
+    const { clientId, vendorId, serviceType, appointmentDate, timeSlot, location} = req.body;
+    
 
     if (!clientId || !vendorId || !serviceType || !appointmentDate || !timeSlot || !location) {
       return res.status(400).json({ message: 'All fields are required' });
